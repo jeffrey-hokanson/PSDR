fea129309fae47cc67baae3442e2387ebbc35258
Jeffrey Hokanson @ Thor <jeffrey@hokanson.us>
2018-09-08 15:16:15 -0600

    Fixed bug with errors

--- psdr/opt/__init__.py
+++ psdr/opt/__init__.py
@@ -1,4 +1,5 @@
 from check_gradient import check_gradient
 from old_util import *
+from shared import LinProgException, InfeasibleConstraints  
 from .minimax import minimax
 from .linprog import linprog
