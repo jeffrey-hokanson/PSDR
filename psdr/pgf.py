6f9fcab1f7a5b596ba928f9b99e84548d2cb6d35
Jeffrey Hokanson <jeffrey@hokanson.us>
2018-09-20 17:14:48 -0600

    Added import functionality to PGF

--- psdr/pgf.py
+++ psdr/pgf.py
@@ -5,6 +5,7 @@ class PGF:
 	def __init__(self):
 		self.column_names = []
 		self.columns = []
+
 	def add(self, name, column):
 		if len(self.columns) > 1:
 			assert len(self.columns[0]) == len(column)
@@ -12,6 +13,12 @@ class PGF:
 		self.columns.append(column)
 		self.column_names.append(name)
 
+	def keys(self):
+		return self.column_names
+
+	def __getitem__(self, key):
+		i = self.column_names.index(key)
+		return self.columns[i]
 
 	def write(self, filename):
 		f = open(filename,'w')
@@ -27,6 +34,18 @@ class PGF:
 
 		f.close()
 
+	def read(self, filename):
+		with open(filename,'r') as f:
+			for i, line in enumerate(f):
+				# Remove the newline and trailing tab if present
+				line = line.replace('\t\n','').replace('\n','')
+				if i == 0:
+					self.column_names = line.split('\t')
+					self.columns = [ [] for name in self.column_names]
+				else:
+					cols = line.split('\t')
+					for j, col in enumerate(cols):
+						self.columns[j].append(float(col))
 
 
 def save_contour(fname, cs, fmt = 'matlab', **kwargs):
