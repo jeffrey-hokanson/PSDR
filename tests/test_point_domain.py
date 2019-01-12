b404d3587e71cada9de41b9ff05b2ef497a0b2f3
Jeffrey Hokanson <jeffrey@hokanson.us>
2019-01-11 18:57:03 -0700

    Fixed bug in test

--- tests/test_point_domain.py
+++ tests/test_point_domain.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import numpy as np
 from psdr import PointDomain
 
@@ -5,5 +6,7 @@ def test_point_domain_isinside():
 	x = np.ones(10)
 	y = np.zeros(10)
 	dom = PointDomain(x)
-	X = np.hstack([x,y])
+	X = np.vstack([x,y])
+	print(X.shape)
+	print(dom.isinside(X))
 	assert np.all(dom.isinside(X) == np.array([True,False]))
