99f10bee16d182ceef8dc8890876db5a5a4e8b30
Jeffrey Hokanson <jeffrey@hokanson.us>
2019-03-14 21:55:49 -0600

    Added Function version of OTL Circuit example

--- psdr/demos/otl_circuit.py
+++ psdr/demos/otl_circuit.py
@@ -1,9 +1,9 @@
 # An example from https://www.sfu.ca/~ssurjano/otlcircuit.html
 
 import numpy as np
-from psdr import BoxDomain
+from psdr import BoxDomain, Function
 
-__all__ = ['build_otl_circuit_domain', 'otl_circuit']
+__all__ = ['build_otl_circuit_domain', 'otl_circuit', 'OTLCircuit']
 
 def build_otl_circuit_domain():
 	# Parameters
@@ -25,7 +25,18 @@ def otl_circuit(x, return_grad = False):
 
 	Vb1 = 12*Rb2/(Rb1 + Rb2)
 	Vm = (Vb1 + 0.74)*beta*(Rc2 + 9)/(beta*(Rc2 + 9)+Rf) + 11.35*Rf/(beta*(Rc2 + 9) + Rf) + 0.74*Rf*beta*(Rc2 + 9)/( (beta*(Rc2 + 9) + Rf)*Rc1)
-	if not return_grad: return Vm
+	
+	return Vm
+
+def otl_circuit_grad(x):
+	x = x.reshape(-1,6)
+	Rb1 = x[:,0]
+	Rb2 = x[:,1]
+	Rf = x[:,2]
+	Rc1 = x[:,3]
+	Rc2 = x[:,4]
+	beta = x[:,5]
+
 	grad = np.vstack([
 			-12*Rb2*beta*(Rc2 + 9)/((Rb1 + Rb2)**2*(Rf + beta*(Rc2 + 9))),
 			12*Rb1*beta*(Rc2 + 9)/((Rb1 + Rb2)**2*(Rf + beta*(Rc2 + 9))),
@@ -34,4 +45,16 @@ def otl_circuit(x, return_grad = False):
 			beta*(-11.35*Rc1*Rf*(Rb1 + Rb2) - Rc1*beta*(0.74*Rb1 + 12.74*Rb2)*(Rc2 + 9) + Rc1*(0.74*Rb1 + 12.74*Rb2)*(Rf + beta*(Rc2 + 9)) - 0.74*Rf*beta*(Rb1 + Rb2)*(Rc2 + 9) + 0.74*Rf*(Rb1 + Rb2)*(Rf + beta*(Rc2 + 9)))/(Rc1*(Rb1 + Rb2)*(Rf + beta*(Rc2 + 9))**2),
 			(Rc2 + 9)*(-11.35*Rc1*Rf*(Rb1 + Rb2) - Rc1*beta*(0.74*Rb1 + 12.74*Rb2)*(Rc2 + 9) + Rc1*(0.74*Rb1 + 12.74*Rb2)*(Rf + beta*(Rc2 + 9)) - 0.74*Rf*beta*(Rb1 + Rb2)*(Rc2 + 9) + 0.74*Rf*(Rb1 + Rb2)*(Rf + beta*(Rc2 + 9)))/(Rc1*(Rb1 + Rb2)*(Rf + beta*(Rc2 + 9))**2),
 		]).T
-	return Vm, grad
+	return grad
+
+
+class OTLCircuit(Function):
+	r"""
+	"""
+	def __init__(self):
+		domain = build_otl_circuit_domain()
+		funs = [otl_circuit]
+		grads = [otl_circuit_grad] 
+
+		Function.__init__(self, funs, domain, grads = grads, vectorized = True)
+
