0eee3e90ccef5a400c62fbabe4d4bf78e7dc81df
Jeffrey Hokanson <jeffrey@hokanson.us>
2019-03-14 23:18:44 -0600

    Testing Lipschitz

--- tests/test_lipschitz.py
+++ tests/test_lipschitz.py
@@ -1,7 +1,39 @@
+from __future__ import print_function
 import numpy as np
+import scipy.linalg
 from psdr import LipschitzMatrix
- 
-def test_L(m = 5):
-	pass
-	# assert np.isclose(np.linalg.norm(lip.M - lip.L.T.dot(lip.L), 'inf'),0)
+from psdr.demos import OTLCircuit
+
+def test_lipschitz_grad(N = 10):
+
+	func = OTLCircuit()
+	X = func.sample(N)
+	grads = func.grad(X)
+	
+	lip = LipschitzMatrix(ftol = 1e-10, gtol = 1e-10, verbose = True)
+	lip.fit(grads = grads)
+	
+	H = np.copy(lip.H)
+
+	for g in grads:
+		gap = np.min(scipy.linalg.eigvalsh(H - np.outer(g,g)))
+		print(gap)
+		assert gap >= -1e-6
 		
+def test_lipschitz_func(M = 20):
+
+	func = OTLCircuit()
+	X = func.sample(M)
+	fX = func(X)
+	
+	lip = LipschitzMatrix(ftol = 1e-10, gtol = 1e-10, verbose = True)
+	lip.fit(X, fX)
+	
+	H = np.copy(lip.H)
+
+	for i in range(M):
+		for j in range(i+1,M):
+			y = X[i] - X[j]
+			gap = y.dot(H.dot(y)) - (fX[i] - fX[j])**2
+			print(gap)
+			assert gap >= -1e-6
