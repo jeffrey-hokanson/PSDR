de51b7dc142eb771347350b3136e4ffecc916809
laksharma30 <laksharma30@gmail.com>
2019-03-24 21:23:43 -0600

    Workflow for deformation and flow solver.

---
+++ psdr/demos/naca0012/workflowbash.py
@@ -0,0 +1,153 @@
+from parallel_computation import parallel_computation
+from mesh_deformation import mesh_deformation
+import subprocess
+import re
+import os
+import copy
+import shutil
+import time
+import numpy as np
+import datetime
+
+
+parameters = [{'kind' : 'HICKS_HENNE', 'param' : (1,0.1), 'value' : 0.1}, {'kind' : 'HICKS_HENNE', 'param' : (0,0.5), 'value' : 0.1}]
+def_file = 'def_NACA0012.cfg'
+
+# for index,param in enumerate(parameters):
+def writeConfigFile(parameters, def_file,path):
+	dv_kind_pattern = r'^DV_KIND='
+	dv_param_pattern = r'^DV_PARAM='
+	dv_value_pattern  = r'^DV_VALUE='
+	with open(def_file, 'r+') as f:
+		lines = f.readlines()
+	tempDefStr = ''
+	for line in lines:
+		if re.search(dv_kind_pattern, line) is not None:
+			tempDefStr += 'DV_KIND= '
+			i = 0
+			for i in range(len(parameters)-1):
+				tempDefStr += parameters[i]['kind'] + ', '
+				i += 1
+			tempDefStr += parameters[i]['kind']
+			tempDefStr += '\n'
+
+		elif re.search(dv_param_pattern, line) is not None:
+			tempDefStr += 'DV_PARAM= '
+			for i in range(len(parameters)-1):
+				tempDefStr += str(parameters[i]['param'])+ '; '
+				i += 1
+			tempDefStr +=str(parameters[i]['param'])
+			tempDefStr += '\n'
+		elif re.search(dv_value_pattern,line) is not None:
+			tempDefStr += 'DV_VALUE= '
+			for i in range(len(parameters)-1):
+				tempDefStr += str(parameters[i]['value']) + ', '
+				i += 1
+			tempDefStr += str(parameters[i]['value'])
+			tempDefStr += '\n'
+		else:
+			tempDefStr += line
+
+	shutil.copy(path + '/def_NACA0012.cfg',path + '/def_NACA0012.cfg.bak')
+	cfg_file = path + '/def_NACA0012.cfg'  
+	with open(cfg_file, 'w+') as f:
+		f.write(tempDefStr)
+
+
+def generateParameters(parameters):
+	numOfParams = len(parameters[0])
+	paramDict = {'kind': 'HICKS_HENNE', 'param': None, 'value' : None}
+	paramRow = []
+	returnParamters = []
+	for i in range(numOfParams):
+		
+
+		tempDict = copy.deepcopy(paramDict)
+		if numOfParams == 1:
+			tempTuple = (1, 0.4)
+		elif i < numOfParams//2:
+			tempTuple = (1 , 0.1 + (i * 0.8/(numOfParams - 1)) )
+		else:
+			tempTuple = (0 , 0.1 + (i * 0.8/(numOfParams - 1)) )
+		tempDict['param'] =  tempTuple
+		paramRow.append(tempDict)
+	for row in (parameters):
+		for index,col in enumerate(row):
+			paramRow[index]['value'] = col
+		returnParamters.append(copy.deepcopy(paramRow))
+	return returnParamters
+
+# parameters  = [[0.1,0.3,0.2,0.1],[0.1,0.2,0.2,0.1],[0.1,0.3,0.2,0.1],[0.1,0.3,0.2,0.1]]
+# parameters = np.asarray(parameters)
+# parameters = [[0.1]]
+
+
+# subprocess.run(['SU2_CFD',inv_cfg])
+
+def workflowbash(parameters):
+	cwd = os.getcwd()
+	inv_cfg = 'inv_NACA0012.cfg'
+	def_cfg = 'def_NACA0012.cfg'
+	inv_def_cfg = 'inv_NACA0012_def.cfg'
+
+	parameters = np.asarray(parameters)
+	parameters_dict_form = generateParameters(parameters)
+	ld_out = np.zeros((parameters.shape[0],2))
+	mydir = os.path.join(os.getcwd(), datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+	os.mkdir(mydir)
+	for iteration,row in enumerate(parameters_dict_form):
+		try:
+
+			path = os.path.join(cwd, str(iteration))
+			os.mkdir(path)
+			# os.mkdir()
+			shutil.copy('mesh_out.su2', path)
+			shutil.copy(def_cfg,path)
+			shutil.copy(inv_def_cfg,path)
+			writeConfigFile(row,def_file,path)				 
+
+			# subprocess.run(['SU2_CFD','inv_NACA0012.cfg'],cwd=path)
+			deformation_out_status = subprocess.run(['SU2_DEF','def_NACA0012.cfg'],cwd=path)
+			# print('def out')
+			# print(deformation_out_status)
+			# time.sleep(5)
+
+			cfd_solver_out_status  = subprocess.run(['SU2_CFD','inv_NACA0012_def.cfg'],cwd=path)
+			# print('cfd out', cfd_solver_out_status)
+			# time.sleep(5)
+			shutil.move(path, mydir)
+			with open(os.path.join(mydir, str(iteration), 'forces_breakdown.dat'), 'r+') as f:
+					lines = f.readlines()
+			cl_pattern = r'Total CL:'
+			cd_pattern = r'Total CD:'
+			cl = cd = 0
+			for line in lines:
+					if re.search(cl_pattern, line) is not None:
+						temp = line.split('Total CL:       ')
+						# print(temp)
+						temp = temp[1].split(' ')
+						cl = float(temp[0].split(' ')[0])
+
+					if re.search(cd_pattern,line) is not None:
+						temp = line.split('Total CD:       ')
+						temp = temp[1].split(' ')
+						cd = float(temp[0].split(' ')[0])
+						break;
+			ld_out[iteration] = [cl, cd]
+
+
+		except(IOError) as e:
+			print('There was a file error : ', e)
+		except(Exception) as e:
+			print('There was an error : ', e)
+
+	return ld_out
+# print(ld_out)
+
+def main():
+	parameters = np.zeros((0,0))
+	workflowbash(parameters)
+
+
+if __name__ == '__main__':
+	main()
