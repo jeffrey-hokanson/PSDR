12c4b505c97c4ab9eba2df30a1a90024e014a744
Jeffrey Hokanson @ Thor <jeffrey@hokanson.us>
2019-04-03 12:27:33 -0600

    Adding python3 install instructions

--- setup.py
+++ setup.py
@@ -1,12 +1,9 @@
 import os
+import sys
 from setuptools import setup
 
 
-setup(name='psdr',
-	version = '0.1',
-	description = 'Parameter Space Dimension Reduction Toolbox',
-	author = 'Jeffrey M. Hokanson',
-	packages = ['psdr', 'psdr.opt', 'psdr.demos'],
+if sys.version_info[0] < 3:
 	install_requires = [
 		'numpy>=1.15', 
 		'scipy', 
@@ -20,5 +17,26 @@ setup(name='psdr',
 		'distributed',
 		# Required for python 2.7
 		'tornado<6.0.0',
-		],
+		]
+else:
+	install_requires = [
+		'numpy>=1.15', 
+		'scipy', 
+		'matplotlib',
+		'redis',
+		'dill',
+		'cvxpy',
+		'cvxopt',
+		'tqdm',
+		'dask',
+		'distributed',
+		]
+
+
+setup(name='psdr',
+	version = '0.1',
+	description = 'Parameter Space Dimension Reduction Toolbox',
+	author = 'Jeffrey M. Hokanson',
+	packages = ['psdr', 'psdr.demos'],
+	install_requires = install_requires,
 	)
