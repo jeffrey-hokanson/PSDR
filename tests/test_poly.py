87a65bd6b47bfe745a7b3360c79f2f24da5edc8c
Jeffrey Hokanson @ Thor <jeffrey@hokanson.us>
2019-04-04 22:04:58 -0600

    Added tests for fitting and other bases

--- tests/test_poly.py
+++ tests/test_poly.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 import numpy as np
 
-from psdr import PolynomialApproximation, PolynomialFunction, LegendreTensorBasis
+from psdr import PolynomialApproximation, PolynomialFunction, LegendreTensorBasis, BoxDomain
 from checkder import *
 
 def test_poly_der(dimension = 3, degree = 5):
@@ -25,3 +25,38 @@ def test_poly_hess(dimension = 3, degree = 5):
 	print(pf.hessian(x))
 
 	assert check_hessian(x, pf.eval, lambda x: pf.hessian(x).reshape(dimension, dimension) ) < 1e-5
+
+
+def test_poly_basis(dimension = 2, degree = 5):
+	""" test different bases"""	
+	coef = np.random.randn(len(LegendreTensorBasis(dimension, degree)))
+	pf = PolynomialFunction(dimension, degree, coef)
+
+	dom = BoxDomain(-np.ones(dimension), np.ones(dimension))
+	X = dom.sample(100)
+	fX = pf(X)
+	Xtest = dom.sample(1000)
+	fXtest = pf(Xtest)
+	
+	for basis in ['legendre', 'monomial', 'chebyshev', 'laguerre', 'hermite']:
+		pa = PolynomialApproximation(degree, basis = basis)
+		pa.fit(X, fX)
+		assert np.linalg.norm(pa(Xtest) - fXtest, np.inf) < 1e-7
+
+
+def test_poly_fit(dimension = 2, degree = 5):
+	dom = BoxDomain(-np.ones(dimension), np.ones(dimension))
+	X = dom.sample(100)
+
+	fXnoise = np.random.randn(X.shape[0])
+
+	for bound in ['lower', 'upper']:
+		for norm in [1,2, np.inf]:
+			for basis in ['legendre', 'monomial', 'chebyshev', 'laguerre', 'hermite']:
+				pa = PolynomialApproximation(degree, basis = basis, norm = norm, bound = bound)
+				pa.fit(X, fXnoise)
+				if bound == 'lower':
+					assert np.all(pa(X) <= fXnoise +1e-7)
+				if bound == 'upper':
+					assert np.all(pa(X) >= fXnoise -1e-7)
+		
