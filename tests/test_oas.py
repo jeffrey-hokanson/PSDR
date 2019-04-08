b407d4776cf26c501f5ed0c2139eb92db2003d44
jeffrey@sigurd <jeffrey@hokanson.us>
2019-04-08 13:11:06 -0600

    Fixed import location for test data

--- tests/test_oas.py
+++ tests/test_oas.py
@@ -1,14 +1,17 @@
 import numpy as np
 from psdr.demos import OpenAeroStruct
-
+import os
 
 def test_oas():
 	oas = OpenAeroStruct()
-	X = np.loadtxt('oas.input')
-	Y = np.loadtxt('oas.output')
+	# Load true values
+	# we do this dirname trick to make sure we can find the test data
+	# https://stackoverflow.com/questions/53719350/travisci-with-pytest-and-numpy-load-file-not-found
+	X = np.loadtxt(os.path.join(os.path.dirname(__file__), 'oas.input'))
+	Y = np.loadtxt(os.path.join(os.path.dirname(__file__), 'oas.output'))
 
 	Ynew = oas(X)
-	assert np.isclose(Y, Ynew), "Outputs did not match"
+	assert np.all(np.isclose(Y, Ynew)), "Outputs did not match"
 
 
 if __name__ == '__main__':
