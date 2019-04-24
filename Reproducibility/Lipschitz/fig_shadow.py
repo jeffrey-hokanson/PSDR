44be20b2e357071f2b1a5e33b015ca759f1093c3
Jeffrey Hokanson @ Thor <jeffrey@hokanson.us>
2019-04-23 22:02:01 -0600

    Rendering in use version

--- Reproducibility/Lipschitz/fig_shadow.py
+++ Reproducibility/Lipschitz/fig_shadow.py
@@ -1,6 +1,7 @@
 import numpy as np
 import psdr
 import psdr.demos
+from psdr.pgf import PGF
 
 np.random.seed(0)
 
@@ -22,14 +23,18 @@ for lip, name in zip([lip_mat, lip_con], ['mat', 'con']):
 	lip.fit(grads = gradX)
 	if name == 'mat':
 		U = lip.U[:,0].copy()
+		pgf = PGF()
+		for i, u in enumerate(U):
+			pgf.add('x%d' % (i+1), [u])
+		pgf.write('data/fig_shadow_U.dat')
 
 	lip.shadow_plot(X, fX, ax = None, U = U, dim = 1, pgfname = 'data/fig_shadow_%s.dat' % (name,) ) 
 	
-	if False:
+	if True:
 		print("computing envelope")
 		lip.shadow_envelope(Xg, fXg, ax = None, pgfname = 'data/fig_shadow_%s_envelope.dat' % (name, ), U = U)
 
-	if False:	
+	if True:	
 		print("computing envelope estimate")
 		lip.shadow_envelope_estimate(fun.domain, X, fX, U = U,
 			pgfname = 'data/fig_shadow_%s_envelope_estimate.dat' % (name,), progress = 2, ngrid = 100)
