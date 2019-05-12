Fixed bug in bounding gradient computation

psdr/lipschitz.py
@@ -584,7 +584,8 @@ class LowerBound(BaseFunction):
 		norms = cdist(y.reshape(1,-1), self.Y, 'euclidean').flatten()
 		G = np.zeros([self.Y.shape[0], x.shape[0]] )
 		I = (norms > 0)
-		G[I,:] = -((y - self.Y[I]).T/norms[I]).T
+		G[I,:] = -(  (y - self.Y[I]).T /norms[I]).T
+		G = self.L.T.dot(G.T).T
 		return G
 
 class UpperBound(BaseFunction):
@@ -604,6 +605,7 @@ class UpperBound(BaseFunction):
 		G = np.zeros([self.Y.shape[0], x.shape[0]] )
 		I = (norms > 0)
 		G[I,:] = -((y - self.Y[I]).T/norms[I]).T
+		G = self.L.T.dot(G.T).T
 		return G
 
 

tests/test_lipschitz.py
@@ -1,7 +1,8 @@
 from __future__ import print_function
 import numpy as np
 import scipy.linalg
-from psdr import LipschitzMatrix, LipschitzConstant, DiagonalLipschitzMatrix
+from checkder import *
+from psdr import LipschitzMatrix, LipschitzConstant, DiagonalLipschitzMatrix, BoxDomain
 from psdr.demos import OTLCircuit
 
 np.random.seed(0)
@@ -93,3 +94,20 @@ def test_solver(N = 50, M = 0):
 	print(np.linalg.norm(H1- H2, 'fro'))
 	assert np.isclose(np.linalg.norm(H1 - H2, 'fro'),0)
 
+def test_set_bounds():
+	from psdr.lipschitz import LowerBound, UpperBound
+	L = np.eye(2)
+	L[1,1] = 0.1
+	dom = BoxDomain([-1,-1],[1,1])
+	X = dom.sample(10)
+	fX = np.random.randn(X.shape[0])
+	
+	lower = LowerBound(L, X, fX)
+	x = dom.sample()
+	err = check_gradient(x, lower.eval, lower.grad) 		
+	assert err < 1e-7, "Gradient error too large"	
+	
+	upper = UpperBound(L, X, fX)
+	x = dom.sample()
+	err = check_gradient(x, lower.eval, lower.grad) 		
+	assert err < 1e-7, "Gradient error too large"	