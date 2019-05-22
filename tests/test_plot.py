cb30074da335c929732b4fe35a927992daef7a63
Jeffrey Hokanson <jeffrey@hokanson.us>
2019-05-21 20:17:33 -0600

    Fixed path for output comparison

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -1,10 +1,14 @@
 from __future__ import print_function
 import filecmp
 import matplotlib
+import os
 matplotlib.use("Agg")
 
 import matplotlib.pyplot as plt
 import psdr, psdr.demos
+
+path = os.path.abspath(os.path.realpath(__file__))
+path = os.path.dirname(path)
 
 def test_shadow():
 	fun = psdr.demos.OTLCircuit()
@@ -21,13 +25,13 @@ def test_shadow():
 	# Generate shadow plot with response surface
 	ax = pra.shadow_plot(X, fX, pgfname = 'test_shadow.dat')
 
-	assert filecmp.cmp('data/test_shadow.dat', 'test_shadow.dat') 
-	assert filecmp.cmp('data/test_shadow_response.dat', 'test_shadow_response.dat') 
+	assert filecmp.cmp(os.path.join(path,'data/test_shadow.dat'), 'test_shadow.dat') 
+	assert filecmp.cmp(os.path.join(path,'data/test_shadow_response.dat'), 'test_shadow_response.dat') 
 
 
 	# Generate shadow envelope
 	pra.shadow_envelope(Xg, fXg, ax = ax, pgfname = 'test_shadow_envelope.dat')
-	assert filecmp.cmp('data/test_shadow_envelope.dat', 'test_shadow_envelope.dat') 
+	assert filecmp.cmp(os.path.join(path, 'data/test_shadow_envelope.dat'), 'test_shadow_envelope.dat') 
 
 	fig, ax2 = plt.subplots()
 
@@ -46,12 +50,26 @@ def test_shadow_lipschitz():
 	
 	ax = lip.shadow_plot(X, fX)
 	lip.shadow_uncertainty(fun.domain, X, fX, ax = ax, ngrid = 4, pgfname = 'test_shadow_uncertainty.dat')
-	assert filecmp.cmp('data/test_shadow_uncertainty.dat', 'test_shadow_uncertainty.dat') 
+	assert filecmp.cmp(os.path.join(path, 'data/test_shadow_uncertainty.dat'), 'test_shadow_uncertainty.dat') 
 
+
+def test_score():
+	fun = psdr.demos.OTLCircuit()
+	X = fun.domain.sample_grid(2)
+	fX = fun(X)
+	grads = fun.grad(X)
+
+	lip = psdr.DiagonalLipschitzMatrix()
+	lip.fit(grads = grads)
+	
+	ax = lip.plot_score(domain = fun.domain)
+	ax.figure.tight_layout()
+	print(lip.U)	
 
 if __name__ == '__main__':
 	#test_shadow_lipschitz()
-	test_shadow()
+	#test_shadow()
+	test_score()
 	plt.show()
 	
 
