92adb88e4dab5c51e913cc84a8e4d8ab3d2e97b5
Jeffrey Hokanson @ Thor <jeffrey@hokanson.us>
2019-05-29 20:15:32 -0600

    Moved random utilies into separate file

---
+++ psdr/misc.py
@@ -0,0 +1,20 @@
+from __future__ import print_function, division
+""" misc. utilities and defintions
+"""
+
+def merge(x, y):
+	z = x.copy()
+	z.update(y)
+	return z
+
+DEFAULT_CVXPY_KWARGS = {
+	'solver': cp.CVXOPT,
+	'reltol': 1e-10,
+	'abstol' : 1e-10,
+	'verbose': False,
+	'kktsolver': 'robust', 
+	'warm_start': True,
+}
+
+class SolverError(ValueError):
+	pass
