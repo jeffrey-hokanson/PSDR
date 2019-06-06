21b60a289c31b0bbaf8c79b90e6d7eed5bc18c10
jeffrey@sigurd <jeffrey@hokanson.us>
2019-06-06 17:45:57 -0600

    Added switch to lower solution tolerances on Travis

--- tests/test_naca.py
+++ tests/test_naca.py
@@ -2,6 +2,8 @@ from __future__ import print_function
 import numpy as np
 import psdr, psdr.demos
 from checkder import check_jacobian
+import os
+import multiprocessing
 
 def test_naca():
 	naca = psdr.demos.NACA0012()
@@ -24,18 +26,27 @@ def test_naca():
 	assert np.all(np.isclose(y, y_true))
 
 def test_naca_grad():
-	# We limit number of dimensions and iteration count 
-	# so entire test takes less than 10min on Travis-CI
-	naca = psdr.demos.NACA0012(n_upper = 1, n_lower = 1, verbose = True, maxiter = 200, nprocesses = 1)	
-	x = 0.2*np.ones(len(naca.domain))	
+	nprocesses = max(1, multiprocessing.cpu_count() - 1)
 
-	if False:	
-		x1 = np.copy(x)
-		fx1, grad = naca(x1, workdir='naca_f1', keep_data = True, return_grad = True)
-	
-	if True:
+	try:
+		travis = os.environ['TRAVIS'] == 'true'
+	except KeyError:
+		travis = False
+
+	if travis:
+		# We limit number of dimensions and iteration count 
+		# so entire test takes less than 10min on Travis-CI
+		naca = psdr.demos.NACA0012(n_upper = 1, n_lower = 1, verbose = True, maxiter = 200, nprocesses = 1)	
+		x = 0.2*np.ones(len(naca.domain))	
 		err = check_jacobian(x, naca.eval, naca.grad, hvec = [ 1e-4])
 		print('maximum error', err)
 		assert err < 5e-2
+	else:
+		naca = psdr.demos.NACA0012(n_upper = 1, n_lower = 1, verbose = True, maxiter = 1000, nprocesses = nprocesses)	
+		x = 0.2*np.ones(len(naca.domain))	
+		err = check_jacobian(x, naca.eval, naca.grad, hvec = [ 1e-4])
+		print('maximum error', err)
+		assert err < 5e-4
+		
 if __name__ == '__main__':
 	test_naca_grad()
