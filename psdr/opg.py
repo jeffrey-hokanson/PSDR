1a6412e421ee37e1b636a6f26dd1073d8469b11a
Jeffrey Hokanson @ Thor <jeffrey@hokanson.us>
2019-06-06 20:13:41 -0600

    Starting to work on OPG

---
+++ psdr/opg.py
@@ -0,0 +1,26 @@
+
+
+import numpy as np
+
+from __future__ import division, print_function
+
+from subspace import SubspaceBasedDimensionReduction
+
+class OuterProductGradient(SubspaceBasedDimensionReduction):
+	r"""
+
+
+
+	References
+	----------
+	.. [HS89]  W. Hardle and T. M. Stoker.
+		Investigating Smooth Multiple Regression by the Method of Average Derivatives
+		J. Am. Stat. Ass. Vol 84, No 408, (1989). pp 986--995, DOI:0.1080/01621459.1989.10478863  	
+	"""
+	def __init__(self, kernel):
+		pass
+
+	def fit(self, X, fX):
+		r""" 
+		"""
+		pass
