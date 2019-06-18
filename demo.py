aa12713a643e486e00163e4c4497fadfcb57da13
Jeffrey Hokanson @ Thor <jeffrey@hokanson.us>
2019-06-18 00:26:06 -0600

    Demo for helping with opg

--- demo.py
+++ demo.py
@@ -1,12 +1,18 @@
 import numpy as np
 import psdr
+import psdr.demos
 from psdr.opg import *
 
-
-X = np.random.randn(100,3)
-fX = np.random.randn(len(X))
-
+fun = psdr.demos.OTLCircuit()
+X = fun.domain.sample(500)
+fX = fun(X).flatten()
 opg = OuterProductGradient()
 opg.fit(X, fX)
 
 
+grads = fun.grad(X)
+act = psdr.ActiveSubspace()
+act.fit(grads)
+
+print('opg', opg.U[:,0])
+print('act', act.U[:,0])
