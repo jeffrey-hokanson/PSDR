4ab58fe9d5f18088950b56380c061c125742fdcf
Jeffrey Hokanson @ Thor <jeffrey@hokanson.us>
2019-10-11 10:46:07 -0600

    Split grid based Poission sampling out

--- psdr/sample/poisson.py
+++ psdr/sample/poisson.py
@@ -3,7 +3,7 @@ import numpy as np
 import itertools
 from scipy.spatial.distance import cdist
 
-def poisson_disk_sample(domain, r, L = None, Ntries = 100, grid = False):
+def poisson_disk_sample(domain, r, L = None, Ntries = 100):
 	r""" Implements a Poisson disk sampling on an arbitrary domain
 
 
@@ -20,40 +20,8 @@ def poisson_disk_sample(domain, r, L = None, Ntries = 100, grid = False):
 		L = np.eye(len(domain))
 
 
-	# Use the grid approach to limit searches
-	# https://www.cs.ubc.ca/~rbridson/docs/bridson-siggraph07-poissondisk.pdf
-	try:
-		if grid is False:
-			raise MemoryError
-		# This radius is chosen so that no more than one point is in each grid cell
-		grid_size = r/np.sqrt(L.shape[0])
-		grid_start = np.zeros(L.shape[0])
-		grid_dims = np.zeros(L.shape[0], dtype = np.int)
-		for i in range(L.shape[0]):
-			c1 = domain.corner(L[i,:])
-			c2 = domain.corner(-L[i,:])
-			if L[i,:].dot(c1) > L[i,:].dot(c2):
-				c1, c2 = c2, c1
-			grid_start[i] = L[i,:].dot(c1)
-			grid_dims[i] = int(np.ceil((L[i,:].dot(c2) - L[i,:].dot(c1))/grid_size))
-
-		grid = np.zeros(grid_dims, dtype = np.uint32)
-	except (MemoryError, ValueError):
-		grid = None	
-
-	def grid_coord(x):
-		""" location of x on the grid"""
-		Lx = L.dot(x)
-		idx = np.floor((Lx - grid_start)/grid_size)
-		return idx.astype(np.int)
-	
-
 	X = [domain.sample()]
 	active = [True]
-	if grid is not None:
-		idx = grid_coord(X[0])
-		print(idx)
-		grid[tuple(idx)] = 1
 
 	while True:
 		# Pick a random active elemenit
@@ -76,34 +44,103 @@ def poisson_disk_sample(domain, r, L = None, Ntries = 100, grid = False):
 			xc = X[i] + ri*p
 
 			if domain.isinside(xc):
-				if grid is not None:
-					# Although this is technically more efficient, traversing over neighbors in Python is expensive
-					idxc = grid_coord(xc)
-					neighbors = []
-					grid_rad = int(2*np.ceil(np.sqrt(L.shape[0])))+1
-					for idx in itertools.product(*[ (j+k for k in range(-grid_rad, grid_rad+1)) for j in idxc]):
-						try:
-							if min(idx) < 0: raise IndexError
-							n = grid[idx]
-							if n != 0:
-								neighbors.append(n-1)
-						except IndexError:
-							pass
-					if len(neighbors) > 0:
-						d = cdist(L.dot(xc).reshape(1,-1), LX[neighbors])
-					else:
-						d = [2*r]
-				else:
-					d = cdist(L.dot(xc).reshape(1,-1), LX)
+				d = cdist(L.dot(xc).reshape(1,-1), LX)
 				
 				if np.min(d) > r:
 					X.append(xc)
 					active.append(True)
 					success = True
-					if grid is not None:
-						grid[tuple(idxc)] = len(X)
 					break
 
+		if not success:
+			active[i] = False
+
+	return np.array(X)
+
+
+def poisson_disk_sample_grid(domain, r, L = None, Ntries = 100):
+	r"""
+
+	References
+	----------
+	[Bri07]_ Fast Poisson Disk Sampling in Arbitrary Dimensions. 
+		Robert Bridson.  Siggraph 2007	 
+		https://www.cs.ubc.ca/~rbridson/docs/bridson-siggraph07-poissondisk.pdf
+	"""	
+	# Use the grid approach to limit searches
+
+	if L is None:
+		L = np.eye(len(domain))
+
+	
+	# This radius is chosen so that no more than one point is in each grid cell
+	grid_size = r/np.sqrt(L.shape[0]) 	# Step 0, Bri07
+	grid_start = np.zeros(L.shape[0])
+	grid_dims = np.zeros(L.shape[0], dtype = np.int)
+	for i in range(L.shape[0]):
+		c1 = domain.corner(L[i,:])
+		c2 = domain.corner(-L[i,:])
+		if L[i,:].dot(c1) > L[i,:].dot(c2):
+			c1, c2 = c2, c1
+		grid_start[i] = L[i,:].dot(c1)
+		grid_dims[i] = int(np.ceil((L[i,:].dot(c2) - L[i,:].dot(c1))/grid_size))
+
+	# Setup the grid
+	grid = np.zeros(grid_dims, dtype = np.uint32)
+
+	def grid_coord(x):
+		""" location of x on the grid"""
+		Lx = L.dot(x).flatten()
+		idx = np.floor((Lx - grid_start)/grid_size)
+		return idx.astype(np.int)
+
+	X = [domain.sample()]
+	active = [True]
+
+	idx = grid_coord(X[0])
+	grid[tuple(idx)] = 1
+	while True:
+		# Pick a random active elemenit
+		try:
+			i = int(np.random.permutation(np.argwhere(active))[0])
+		except IndexError:
+			break
+		
+		LX = L.dot(np.array(X).T).T
+		print(active)
+		success = False
+		for it in range(Ntries):
+			# Generate a random direction
+			p = domain.random_direction(X[i])
+			p /= np.linalg.norm(L.dot(p))
+			# TODO: Change distribution to sample annulus uniformly
+			ri = np.random.uniform(r, 2*r)
+	
+			# Generate trial point
+			xc = X[i] + ri*p
+			if domain.isinside(xc):
+				idxc = grid_coord(xc)
+				neighbors = []
+				grid_rad = int(2*np.ceil(np.sqrt(L.shape[0])))+1
+				for idx in itertools.product(*[ (j+k for k in range(-grid_rad, grid_rad+1)) for j in idxc]):
+					try:
+						if min(idx) < 0: raise IndexError
+						n = grid[idx]
+						if n != 0:
+							neighbors.append(n-1)
+					except IndexError:
+						pass
+				if len(neighbors) > 0:
+					d = cdist(L.dot(xc).reshape(1,-1), LX[neighbors])
+				else:
+					d = [2*r]
+			
+				if np.min(d) > r:
+					X.append(xc)
+					active.append(True)
+					success = True
+					grid[tuple(idxc)] = len(X)
+					break
 		if not success:
 			active[i] = False
 
--- tests/sample/test_poisson.py
+++ tests/sample/test_poisson.py
@@ -11,7 +11,16 @@ def test_poisson_disk_sample(m = 2, r = 0.3):
 	d = np.nanmin(D, axis = 1)
 	print(d)
 	assert np.all(d >= r) and np.all(d <= 2*r)
+
+def test_poisson_disk_sample_grid(m = 2, r = 0.6):
+	dom = psdr.BoxDomain(-np.ones(m), np.ones(m))
+	X = psdr.poisson_disk_sample_grid(dom, r)
+	D = squareform(pdist(X))
+	D += np.diag(np.nan*np.ones(D.shape[0]))
+	d = np.nanmin(D, axis = 1)
+	print(d)
+	assert np.all(d >= r) and np.all(d <= 2*r)
 	
 
 if __name__ == '__main__':
-	test_poisson_disk_sample()	
+	test_poisson_disk_sample_grid()	
