e4ffd85d071ca83b3b503b4527390a3f0a6714b5
Jeffrey Hokanson <jeffrey@hokanson.us>
2019-10-10 18:02:58 -0600

    Trying to increase coverage

--- tests/domains/test_euclidean.py
+++ tests/domains/test_euclidean.py
@@ -8,5 +8,17 @@ def test_is_unbounded():
 	dom = psdr.LinQuadDomain(lb = -np.inf*np.ones(m), ub = np.inf*np.ones(m))
 	assert dom.is_unbounded is True
 
+def test_corner_unbounded():
+	m = 5
+	dom = psdr.LinQuadDomain(lb = -np.inf*np.ones(m), ub = np.inf*np.ones(m))
+	p = np.ones(m)
+	try:
+		dom.corner(p)
+	except psdr.UnboundedDomainException:
+		pass
+	except:
+		raise Exception("wrong error")
+
+
 if __name__ == '__main__':
 	test_is_unbounded()
