Fixed pdf computation

psdr/domains/uniform.py
@@ -11,4 +11,4 @@ class UniformDomain(BoxDomain, RandomDomain):
 	
 	def _pdf(self, X):
 		area = np.prod([ (ub - lb) for lb, ub in zip(self.lb, self.ub)])
-		return np.ones(X.shape[0])/area
+		return self.isinside(X).astype(np.float)/area

tests/domains/test_normal.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 import numpy as np
 import psdr
 
-def test_pdf(m = 3):
+def test_pdf_full(m = 3):
 	cov = np.eye(m)
 	mean = np.zeros(m) 
 	dom = psdr.NormalDomain(mean, cov)
@@ -12,3 +12,15 @@ def test_pdf(m = 3):
 	p = np.sum(w*dom.pdf(X))
 	print(p)
 	assert np.isclose(p, 1.)
+
+
+def test_pdf_truncate(m = 3):
+	cov = np.eye(m)
+	mean = np.zeros(m) 
+	dom = psdr.NormalDomain(mean, cov, truncate = 5e-1)
+	print(dom.norm_lb)
+	dom_box = psdr.BoxDomain(dom.norm_lb, dom.norm_ub)
+	X, w = dom_box.quadrature_rule(1e6)
+	p = np.sum(w*dom.pdf(X))
+	print(p)
+	assert np.isclose(p, 1., rtol = 1e-3)