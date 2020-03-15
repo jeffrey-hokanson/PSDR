122f99e497273e87756760d33f889ac271524811
Jeffrey Hokanson <jeffrey@hokanson.us>
2020-03-15 15:07:01 -0400

    Renaming to be degree/dim versus p/n

--- psdr/basis.py
+++ psdr/basis.py
@@ -80,9 +80,9 @@ class PolynomialTensorBasis(Basis):
 
 	Parameters
 	----------
-	n: int
+	dim: int
 		The input dimension of the space
-	p: int
+	degree: int
 		The total degree of polynomials
 	polyvander: function
 		Function providing the scalar Vandermonde matrix (i.e., numpy.polynomial.polynomial.polyvander)
@@ -90,12 +90,12 @@ class PolynomialTensorBasis(Basis):
 		Function providing the derivatives of scalar polynomials (i.e., numpy.polynomial.polynomial.polyder)	
  
 	"""
-	def __init__(self, n, p, polyvander, polyder):
-		self.n = n
-		self.p = p
+	def __init__(self, dim, degree, polyvander, polyder):
+		self.dim = int(dim)
+		self.degree = int(degree)
 		self.vander = polyvander
 		self.der = polyder
-		self.indices = index_set(p, n).astype(int)
+		self.indices = index_set(self.degree, self.dim).astype(int)
 		self._build_Dmat()
 
 	def __len__(self):
@@ -104,9 +104,9 @@ class PolynomialTensorBasis(Basis):
 	def _build_Dmat(self):
 		""" Constructs the (scalar) derivative matrix
 		"""
-		self.Dmat = np.zeros( (self.p+1, self.p))
-		for j in range(self.p + 1):
-			ej = np.zeros(self.p + 1)
+		self.Dmat = np.zeros( (self.degree+1, self.degree))
+		for j in range(self.degree + 1):
+			ej = np.zeros(self.degree + 1)
 			ej[j] = 1.
 			self.Dmat[j,:] = self.der(ej)
 
@@ -160,16 +160,16 @@ class PolynomialTensorBasis(Basis):
 		V: np.array
 			Vandermonde matrix
 		"""
-		X = X.reshape(-1, self.n)
+		X = X.reshape(-1, self.dim)
 		X = self._scale(np.array(X))
 		M = X.shape[0]
-		assert X.shape[1] == self.n, "Expected %d dimensions, got %d" % (self.n, X.shape[1])
-		V_coordinate = [self.vander(X[:,k], self.p) for k in range(self.n)]
+		assert X.shape[1] == self.dim, "Expected %d dimensions, got %d" % (self.dim, X.shape[1])
+		V_coordinate = [self.vander(X[:,k], self.degree) for k in range(self.dim)]
 		
 		V = np.ones((M, len(self.indices)), dtype = X.dtype)
 		
 		for j, alpha in enumerate(self.indices):
-			for k in range(self.n):
+			for k in range(self.dim):
 				V[:,j] *= V_coordinate[k][:,alpha[k]]
 		return V
 
@@ -194,7 +194,7 @@ class PolynomialTensorBasis(Basis):
 		Vc: np.array (M,)
 			Product of Vandermonde matrix and :math:`\mathbf c`
 		"""
-		X = X.reshape(-1, self.n)
+		X = X.reshape(-1, self.dim)
 		X = self._scale(np.array(X))
 		M = X.shape[0]
 		c = np.array(c)
@@ -206,14 +206,14 @@ class PolynomialTensorBasis(Basis):
 			c = c.reshape(-1,1)
 			oneD = True
 
-		V_coordinate = [self.vander(X[:,k], self.p) for k in range(self.n)]
+		V_coordinate = [self.vander(X[:,k], self.degree) for k in range(self.dim)]
 		out = np.zeros((M, c.shape[1]))	
 		for j, alpha in enumerate(self.indices):
 
 			# If we have a non-zero coefficient
 			if np.max(np.abs(c[j,:])) > 0.:
 				col = np.ones(M)
-				for ell in range(self.n):
+				for ell in range(self.dim):
 					col *= V_coordinate[ell][:,alpha[ell]]
 
 				for k in range(c.shape[1]):
@@ -247,13 +247,13 @@ class PolynomialTensorBasis(Basis):
 			Derivative of Vandermonde matrix where :code:`Vp[i,j,:]`
 			is the gradient of :code:`V[i,j]`. 
 		"""
-		X = X.reshape(-1, self.n)
+		X = X.reshape(-1, self.dim)
 		X = self._scale(np.array(X))
 		M = X.shape[0]
-		V_coordinate = [self.vander(X[:,k], self.p) for k in range(self.n)]
+		V_coordinate = [self.vander(X[:,k], self.degree) for k in range(self.dim)]
 		
 		N = len(self.indices)
-		DV = np.ones((M, N, self.n), dtype = X.dtype)
+		DV = np.ones((M, N, self.dim), dtype = X.dtype)
 
 		try:
 			dscale = self._dscale()
@@ -261,9 +261,9 @@ class PolynomialTensorBasis(Basis):
 			dscale = np.ones(X.shape[1])	
 
 
-		for k in range(self.n):
+		for k in range(self.dim):
 			for j, alpha in enumerate(self.indices):
-				for q in range(self.n):
+				for q in range(self.dim):
 					if q == k:
 						DV[:,j,k] *= np.dot(V_coordinate[q][:,0:-1], self.Dmat[alpha[q],:])
 					else:
@@ -298,13 +298,13 @@ class PolynomialTensorBasis(Basis):
 			Second derivative of Vandermonde matrix where :code:`Vpp[i,j,:,:]`
 			is the Hessian of :code:`V[i,j]`. 
 		"""
-		X = X.reshape(-1, self.n)
+		X = X.reshape(-1, self.dim)
 		X = self._scale(np.array(X))
 		M = X.shape[0]
-		V_coordinate = [self.vander(X[:,k], self.p) for k in range(self.n)]
+		V_coordinate = [self.vander(X[:,k], self.degree) for k in range(self.dim)]
 		
 		N = len(self.indices)
-		DDV = np.ones((M, N, self.n, self.n), dtype = X.dtype)
+		DDV = np.ones((M, N, self.dim, self.dim), dtype = X.dtype)
 
 		try:
 			dscale = self._dscale()
@@ -312,13 +312,13 @@ class PolynomialTensorBasis(Basis):
 			dscale = np.ones(X.shape[1])	
 
 
-		for k in range(self.n):
-			for ell in range(k, self.n):
+		for k in range(self.dim):
+			for ell in range(k, self.dim):
 				for j, alpha in enumerate(self.indices):
-					for q in range(self.n):
+					for q in range(self.dim):
 						if q == k == ell:
 							# We need the second derivative
-							eq = np.zeros(self.p+1)
+							eq = np.zeros(self.degree+1)
 							eq[alpha[q]] = 1.
 							der2 = self.der(eq, 2)
 							DDV[:,j,k,ell] *= V_coordinate[q][:,0:len(der2)].dot(der2)
@@ -333,7 +333,7 @@ class PolynomialTensorBasis(Basis):
 		return DDV
 
 	def roots(self, coef):
-		if self.n > 1:
+		if self.dim > 1:
 			raise NotImplementedError
 		r = legroots(coef)
 		return r*(self._ub[0] - self._lb[0])/2.0 + (self._ub[0] + self._lb[0])/2.
@@ -341,11 +341,11 @@ class PolynomialTensorBasis(Basis):
 
 class MonomialTensorBasis(PolynomialTensorBasis):
 	"""A tensor product basis of bounded total degree built from the monomials"""
-	def __init__(self, n, p):
-		PolynomialTensorBasis.__init__(self, n, p, polyvander, polyder)
+	def __init__(self, dim, degree):
+		PolynomialTensorBasis.__init__(self, dim, degree, polyvander, polyder)
 	
 	def roots(self, coef):
-		if self.n > 1:
+		if self.dim > 1:
 			raise NotImplementedError
 		r = polyroots(coef)
 		return r*(self._ub[0] - self._lb[0])/2.0 + (self._ub[0] + self._lb[0])/2.
@@ -354,18 +354,18 @@ class LegendreTensorBasis(PolynomialTensorBasis):
 	"""A tensor product basis of bounded total degree built from the Legendre polynomials
 
 	"""
-	def __init__(self, n, p):
-		PolynomialTensorBasis.__init__(self, n, p, legvander, legder)
+	def __init__(self, dim, degree):
+		PolynomialTensorBasis.__init__(self, dim, degree, legvander, legder)
 
 class ChebyshevTensorBasis(PolynomialTensorBasis):
 	"""A tensor product basis of bounded total degree built from the Chebyshev polynomials
 	
 	"""
-	def __init__(self, n, p):
-		PolynomialTensorBasis.__init__(self, n, p, chebvander, chebder)
+	def __init__(self, dim, degree):
+		PolynomialTensorBasis.__init__(self, dim, degree, chebvander, chebder)
 	
 	def roots(self, coef):
-		if self.n > 1:
+		if self.dim > 1:
 			raise NotImplementedError
 		r = chebroots(coef)
 		return r*(self._ub[0] - self._lb[0])/2.0 + (self._ub[0] + self._lb[0])/2.
@@ -374,11 +374,11 @@ class LaguerreTensorBasis(PolynomialTensorBasis):
 	"""A tensor product basis of bounded total degree built from the Laguerre polynomials
 
 	"""
-	def __init__(self, n, p):
-		PolynomialTensorBasis.__init__(self, n, p, lagvander, lagder)
+	def __init__(self, dim, degree):
+		PolynomialTensorBasis.__init__(self, dim, degree, lagvander, lagder)
 	
 	def roots(self, coef):
-		if self.n > 1:
+		if self.dim > 1:
 			raise NotImplementedError
 		r = lagroots(coef)
 		return r*(self._ub[0] - self._lb[0])/2.0 + (self._ub[0] + self._lb[0])/2.
@@ -387,8 +387,8 @@ class HermiteTensorBasis(PolynomialTensorBasis):
 	"""A tensor product basis of bounded total degree built from the Hermite polynomials
 
 	"""
-	def __init__(self, n, p):
-		PolynomialTensorBasis.__init__(self, n, p, hermvander, hermder)
+	def __init__(self, dim, degree):
+		PolynomialTensorBasis.__init__(self, dim, degree, hermvander, hermder)
 
 	def _set_scale(self, X):
 		self._mean = np.mean(X, axis = 0)
@@ -407,7 +407,7 @@ class HermiteTensorBasis(PolynomialTensorBasis):
 			raise NotImplementedError
 
 	def roots(self, coef):
-		if self.n > 1:
+		if self.dim > 1:
 			raise NotImplementedError
 		r = hermroots(coef)
 		return r*self._std[0]*np.sqrt(2) + self._mean[0]
