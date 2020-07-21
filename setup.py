c5a65635141d96dc6715add1a25bff104b640140
Jeffrey Hokanson @ Thor <jeffrey@hokanson.us>
2020-07-21 11:15:44 -0600

    Setting up auto deploy

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ install_requires += [
 
 
 setup(name='psdr',
-	version = '0.3.5',
+	version = '0.3.6',
 	description = 'Parameter Space Dimension Reduction Toolbox',
 	author = 'Jeffrey M. Hokanson',
 	packages = ['psdr', 'psdr.demos', 'psdr.domains', 'psdr.sample', 'psdr.geometry'],
