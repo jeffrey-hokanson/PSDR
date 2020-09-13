b1f9c9fa9ab7991e46691224ef0fe82c3798045e
Jeffrey Hokanson @ Thor <jeffrey@hokanson.us>
2020-09-13 17:22:57 -0600

    Build a response surface based on the Lipschitz central approximation

--- psdr/lipschitz_approx.py
+++ psdr/lipschitz_approx.py
@@ -3,6 +3,8 @@ import cvxpy as cp
 from .lipschitz import LipschitzMatrix
 from .ridge import RidgeFunction
 from .misc import merge
+from .sample import maximin_design_1d
+from .pgf import PGF
 
 
 DEFAULT_CVXPY_KWARGS = {
@@ -43,23 +45,77 @@ def lipschitz_approximation_compatible_data(L, X, fX, norm = 1, **kwargs):
 
 
 class LipschitzApproximation(LipschitzMatrix, RidgeFunction):
-	def __init__(self, L, norm = 1):
+	def __init__(self, L, domain, norm = 1):
 		self._L = np.copy(L)
 		self.norm = norm
+		self.domain = domain
 
 		# Compute the active subspace
 		U, s, VT = np.linalg.svd(L, full_matrices = False)
 		self._U = VT.T
+		self._dimension = self._U.shape[0]
 
 	def fit(self, X, fX):
 		r""" 
 
 		""" 
 		self._X = np.copy(X)
-		self._fX = lipschitz_approximation_compatible_data(self.L, X, fX, norm = self.norm) 
-		self.epsilon = np.max(np.abs(self.fX - fX))	
+		self._fX = np.copy(fX)
+		assert np.all(self.domain.isinside(X)), "Samples outside of the domain"
+		self._y = lipschitz_approximation_compatible_data(self.L, X, fX, norm = self.norm) 
+		self.epsilon = np.max(np.abs(self._y - self._fX))	
 
 	def eval(self, X):
-		lb, ub = self.uncertainty(self.X, self.fX, X)
+		lb, ub = self.uncertainty(self._X, self._y, X)
 		return 0.5*(lb + ub)
 
+
+	def shadow_plot(self, dim = 1, ax = 'auto', pgfname = None, nsamp = 200, U = None):
+		
+		if ax == 'auto':
+			import matplotlib.pyplot as plt
+			if dim == 1:
+				fig, ax = plt.subplots(figsize = (6,6))
+			else:
+				# Hack so that plot is approximately square after adding colorbar 
+				fig, ax = plt.subplots(figsize = (7.5,6))
+		
+		if U is None:
+			U = self.U[:,:dim]
+
+		if dim == 1:
+			assert U.shape[1] == 1, "Must be a one-dimensional ridge function"
+
+			# Plot the central estimate and uncertainty
+			X = maximin_design_1d(self.domain, nsamp, L = self.L) 
+			
+			lb, ub = self.uncertainty(self._X, self._y, X)
+			c = 0.5*(lb+ub)
+			xx = (U.T @ X.T).flatten()
+		
+			if ax is not None:
+				
+				# points
+				ax.plot( (U.T @ self.X.T).flatten(), self._fX,'k.')
+				# central approximation
+				ax.plot(xx, c, 'r')
+				# uncertainty
+				ax.fill_between(xx, lb, ub, alpha = 0.3, color = 'blue')
+	
+			if pgfname is not None:
+				pgf = PGF()
+				pgf.add('x', xx)
+				pgf.add('lb', lb)
+				pgf.add('c', c)
+				pgf.add('ub', ub)
+				pgf.write(pgfname + '_curves.dat')
+
+				pgf = PGF()
+				pgf.add('x', (U.T @ self.X.T).flatten())
+				pgf.add('y', self._fX)
+				pgf.write(pgfname + '_points.dat')	 			
+			
+			return ax		
+		
+
+		 
