ed2337d9811fa59a0b5f2ec2364997e6c0d0b71d
Jeffrey Hokanson @ Thor <jeffrey@hokanson.us>
2020-10-13 18:08:05 -0600

    Unit tests for the previous commit

--- tests/domains/test_convexhull.py
+++ tests/domains/test_convexhull.py
@@ -1,7 +1,8 @@
 import numpy as np
 import psdr
-
-
+import scipy.linalg
+from scipy.linalg import orth
+import pytest
 
 def test_sample():
 	np.random.seed(0)
@@ -72,5 +73,38 @@ def test_tensor():
 	assert np.all(np.isclose(x, [1,1,1]))
 
 
+@pytest.mark.parametrize("m", [3, 5])
+@pytest.mark.parametrize("n", [1,2])
+def test_A_eq(m,n):
+	np.random.seed(0)
+	X = np.random.randn(10+m, m)
+	Q = np.random.randn(m,n)
+	Q = orth(Q)
+	# Orthogonalize against this direction so these points are on a low-dim space
+	X = X - (X @ Q) @ Q.T
+	
+	dom = psdr.ConvexHullDomain(X)
+	Qeq = dom._A_eq_basis	
+	ang = scipy.linalg.subspace_angles(Qeq, Q) 
+	print(ang)
+	assert np.all(np.isclose(ang, 0))
+
+
+
+@pytest.mark.parametrize("m", [2,3,4,5])
+@pytest.mark.parametrize("nullspace_dim", [0,1,2,3])
+def test_A_eq_deficient(m, nullspace_dim):
+	if nullspace_dim >= m: return
+
+	X = np.random.randn(m - nullspace_dim+1, m)
+	dom = psdr.ConvexHullDomain(X)
+	Qeq = dom._A_eq_basis
+	print(Qeq.shape)
+	assert Qeq.shape[1] == nullspace_dim	
+
+
+
 if __name__ == '__main__':
-	test_sample()
+	#test_sample()
+	#test_A_eq(4, 2)
+	test_A_eq_deficient(4, 0)
